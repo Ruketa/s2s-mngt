61d3d5e716a3ad007586dd7222f220934f59658b
Ruketa <tkrkrhr@gmail.com>
2021-12-18 09:32:31 +0900

    added prisma

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,26 @@
 import Layout from "../components/layout"
 import Sidebar from "../components/sidebar"
+import type { GetServerSideProps} from 'next'
+import prisma from "../lib/prisma"
 
-export default function Index() {
+type Props = {
+  count: number;
+};
+
+export const getServerSideProps: GetServerSideProps<Props> = async(ctx) => {
+  const count = await prisma.presentation_plans.count();
+  return {
+    props: {
+      count
+    }
+  }
+}
+
+export default function Index(props: Props) {
   return (
     <section>
       <h1>Questionnaier</h1>
+      <div>questionnaier count: {props.count}</div>
     </section>
   )
 }
--- src/pages/session.tsx
+++ src/pages/session.tsx
@@ -1,18 +1,53 @@
 import Layout from "../components/layout"
 import Sidebar from "../components/sidebar"
+import type { GetServerSideProps} from 'next'
+import prisma from "../lib/prisma"
 
-export default function Session() {
+type Props = {
+  plans: Array<{
+    id: string
+    presenter: string,
+    presentation_title: string,
+    scheduled_on: string,
+    division: string,
+  }>
+}
+
+export const getServerSideProps: GetServerSideProps<Props>  = async() => {
+  const rawData = await prisma.presentation_plans.findMany()
+  const plans = JSON.parse(JSON.stringify(rawData)) 
+  return {
+    props: {
+      plans
+    }
+  }
+}
+
+export default function Session(props: Props) {
   return (
     <section>
-      <h1>Session</h1>
-      <select>
-        <option value="1">Session 1</option>
-        <option value="2">Session 2</option>
-        <option value="3">Session 3</option>
-        <option value="4">Session 4</option>
-      </select>
-      <input type="date" />
-      <button>Register</button>
+      <h1 className="text-3xl">Session</h1>
+      <div className="grid grid-cols-2 gap-2 m-2">
+        <div className="m-2 bg-blue-300">発表タイトル</div>
+        <div>
+          <select className="m-2">
+            {
+              props.plans.map(plan => {
+                return <option key={plan.id} value={plan.id}>{plan.presentation_title}</option>
+              })
+            }
+          </select>
+        </div>
+        <div className="m-2 bg-blue-300">開催日</div>
+        <div className="m-2">
+          <input type="date" placeholder="開催日"/>
+        </div>
+        <div className="m-2 bg-blue-300">開催回</div>
+        <div className="m-2">
+          <input type="number" placeholder="開催回番号"/>
+        </div>
+      </div>
+      <button className="row-span-2">Register</button>
     </section>
   )
 }
